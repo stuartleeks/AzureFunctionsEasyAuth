71806c0003755a2e05a4091b3a76ed5e1cd63bdb
Stuart Leeks <stuartle@microsoft.com>
2018-02-15 11:27:58 +0000

    Add GetClaims

--- src/FunctionWithAuth/AuthFunctions.cs
+++ src/FunctionWithAuth/AuthFunctions.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -14,13 +15,31 @@ namespace FunctionWithAuth
         [FunctionName("IsAuthenticated")]
         public static HttpResponseMessage IsAuthenticated(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
-            HttpRequestMessage req, 
+            HttpRequestMessage request, 
             TraceWriter log)
         {
             // simple test to check whether we have an authenticated user
             bool authenticated = Thread.CurrentPrincipal.Identity.IsAuthenticated;
 
-            return req.CreateResponse(HttpStatusCode.OK, authenticated, "application/json");
+            return request.CreateResponse(HttpStatusCode.OK, authenticated, "application/json");
+        }
+
+        [FunctionName("GetClaims")]
+        public static HttpResponseMessage GetClaims(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+            HttpRequestMessage request,
+            TraceWriter log)
+        {
+            if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            {
+                var claimsPrincipal = (ClaimsPrincipal)Thread.CurrentPrincipal;
+                var claims = claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+                return request.CreateResponse(HttpStatusCode.OK, claims, "application/json");
+            }
+            else
+            {
+                return request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Not Authorized");
+            }
         }
     }
 }
