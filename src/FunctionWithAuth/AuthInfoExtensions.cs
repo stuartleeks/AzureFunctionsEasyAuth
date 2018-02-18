c5c076fe2ee5fcc3f34f4ee631de41218dd1f778
Stuart Leeks <stuartle@microsoft.com>
2018-02-18 20:22:54 +0000

    Add more comments

--- src/FunctionWithAuth/AuthInfoExtensions.cs
+++ src/FunctionWithAuth/AuthInfoExtensions.cs
@@ -8,13 +8,24 @@ namespace FunctionWithAuth
 {
     public static class AuthInfoExtensions
     {
-        private static HttpClient _httpClient = new HttpClient();
+        private static HttpClient _httpClient = new HttpClient(); // cache and reuse to avoid repeated creation on Function calls
 
+        /// <summary>
+        /// Find a claim of the specified type
+        /// </summary>
+        /// <param name="authInfo"></param>
+        /// <param name="claimType"></param>
+        /// <returns></returns>
         public static AuthUserClaim GetClaim(this AuthInfo authInfo, string claimType)
         {
             return authInfo.UserClaims.FirstOrDefault(c => c.Type == claimType);
         }
 
+        /// <summary>
+        /// Get the EasyAuth properties for the currently authenticated user
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public static async Task<AuthInfo> GetAuthInfoAsync(this HttpRequestMessage request)
         {
             string zumoAuthToken = request.GetZumoAuthToken();
@@ -35,8 +46,9 @@ namespace FunctionWithAuth
         }
         private static string GetEasyAuthEndpoint()
         {
+            // Get the hostname from environment variables so that we don't need config - thank you App Service!
             var hostname = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
-
+            // Build up the .auth/me url
             string requestUri = $"https://{hostname}/.auth/me";
             return requestUri;
         }
